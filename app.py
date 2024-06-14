de9c091d442a5025e8dc8b62036a8c1446d79067
Emmanuel Ezeokeke <147157655+Emarhnuel@users.noreply.github.com>
2024-06-14 13:15:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,19 @@
 import os
 import streamlit as st
 from dotenv import load_dotenv
-from langchain_community.document_loaders import UnstructuredMarkdownLoader
+from langchain.document_loaders import UnstructuredMarkdownLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_openai import OpenAIEmbeddings, ChatOpenAI
-from langchain_community.vectorstores import FAISS
-from langchain.prompts.chat import (ChatPromptTemplate,
-                                    HumanMessagePromptTemplate,
-                                    SystemMessagePromptTemplate)
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.prompts import PromptTemplate
 from langchain.retrievers import MultiQueryRetriever
-from langchain.chains import RetrievalQA
+from langchain.chains import RetrievalQA,  ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
+from langchain.chat_models import ChatOpenAI
 
 # Load environment variables
 load_dotenv()
-openai_api_key = os.getenv("OPENAI_API_KEY")
-os.environ["OPENAI_API_KEY"] = openai_api_key
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 
 # Define the chatbot function
 def main():
@@ -29,34 +28,61 @@ def main():
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
     documents = text_splitter.split_documents(documents)
 
+    # Embed and store the document chunks
     embeddings = OpenAIEmbeddings(model="text-embedding-ada-002")
     vector_store = FAISS.from_documents(documents, embeddings)
-    retriever = vector_store.as_retriever()
+    retriever = vector_store.as_retriever(search_type="similarity", search_kwargs={"k": 4})
 
     # Define the prompt template
-    messages = [
-        SystemMessagePromptTemplate(
-            content="You are a helpful and informative chatbot about Churchill Insurance policies. "
-                    "Use your knowledge to answer the user's questions based on the provided context."
-        ),
-        HumanMessagePromptTemplate(content="{question}"),
-    ]
+    prompt_template = """
+    <|im_start|>system
+    You are a helpful assistant.
+    You are given relevant documents for context and a question. Provide a conversational answer.
+    If you don't know the answer, just say "I do not know." Don't make up an answer.
+    <|im_end|>
+    <|im_start|>user
+    {question}
+    <|im_end|>
+    <|im_start|>assistant
+    {context} 
+    <|im_end|>
+    """
+    PROMPT = PromptTemplate(
+        template=prompt_template, input_variables=["context", "question"]
+    )
 
-    # Create the prompt
-    prompt = ChatPromptTemplate.from_messages(messages)
-
-    # Set up the LLM and retrieval chain
+    # Set up the LLM
     llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
-    # Create the chain
+
+    #Conversational Memory
+    memory = ConversationBufferMemory(memory_key="chat_history")
+
     retriever_from_llm = MultiQueryRetriever.from_llm(retriever, llm=llm)
-    qa_chain = RetrievalQA.from_chain_type(retriever_from_llm, prompt)
+
+    # Create the chain
+    qa_chain = ConversationalRetrievalChain.from_llm(llm=llm,
+                                                     chain_type="stuff",
+                                                     retriever=retriever_from_llm,
+                                                     memory=memory,
+                                                     combine_docs_chain_kwargs={"prompt": PROMPT})
 
     # Streamlit input and interaction
-    user_input = st.text_input("Enter your question here:")
-    if user_input:
-        response = qa_chain(user_input)
-        st.write(response)
+    if "messages" not in st.session_state:
+        st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
+
+    if prompt := st.chat_input():
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.write(prompt)
+
+        with st.chat_message("assistant"):
+            response = qa_chain({"question": prompt})
+            st.write(response["answer"])
+            st.session_state.messages.append({"role": "assistant", "content": response['answer']})
 
 if __name__ == "__main__":
     main()
